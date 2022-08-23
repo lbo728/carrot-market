2f0305147d1fc810ebefb0242868fc7c75bf1483
byungsker <72309817+lbo728@users.noreply.github.com>
2022-08-23 23:11:32 +0900

    #9.1

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,31 +4,18 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  if (phone) {
-    let user;
-    user = await client.user.upsert({
-      where: {
-        phone: +phone,
-      },
-      create: {
-        name: "Anonymous",
-        phone: +phone,
-      },
-      update: {},
-    });
-  } else if (email) {
-    let user;
-    user = await client.user.upsert({
-      where: {
-        email,
-      },
-      create: {
-        name: "Anonymous",
-        email,
-      },
-      update: {},
-    });
-  }
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   /*   if (email) {
     user = await client.user.findUnique({
       where: {
